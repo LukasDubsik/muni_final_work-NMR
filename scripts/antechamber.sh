212b5592c3f28a062dfa8c91cbf0958a545d135c
LukasDubsik <luke@bubaci.net>
2025-09-13 17:36:25 +0200

    Basic scripts organisation

---
+++ scripts/antechamber.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env infinity-env
+
+module add amber
+
+antechamber -i cys.mol2 -fi mol2 -o cys_charges.mol2 -fo mol2 -c bcc -nc 0 -at gaff
---
+++ scripts/parmchk2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env infinity-env
+
+module add amber
+
+parmchk2 -i cys_charges.mol2 -f mol2 -o cys.frcmod
