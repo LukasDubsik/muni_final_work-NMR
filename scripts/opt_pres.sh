515ed347bd5e17f5633c20bcf2b120ce19037063
LukasDubsik <luke@bubaci.net>
2025-09-17 19:03:43 +0200

    Newer versions of the .in and .sh

---
+++ scripts/opt_pres.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env infinity-env
+
+module add amber
+
+pmemd -O -i opt_pres.in -p ${name}.parm7 -c ${name}_opt_temp.rst7 -ref ${name}_opt_pres.rst7 -o opt_pres.out -r ${name}_opt_pres.rst7 -x opt_pres.mdcrd -inf opt_pres.mdinfo
