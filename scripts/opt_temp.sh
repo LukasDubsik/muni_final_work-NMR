004fa18502de9a2299214bad14138fa9caccc98b
Lukas Dubsik <lukasdubsik@wolf17>
2025-09-20 09:15:00 +0200

    Restructure

--- scripts/opt_temp.sh
+++ scripts/opt_temp.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env infinity-env
 
 module add amber
+module add pmemd-cuda
 
-pmemd -O -i opt_temp.in -p ${name}.parm7 -c ${name}_opt_all.rst7 -ref ${name}_opt_all.rst7 -o opt_temp.out -r ${name}_opt_temp.rst7 -x opt_temp.mdcoord -inf opt_temp.mdinfo
+pmemd.cuda -O -i opt_temp.in -p ${name}.parm7 -c ${name}_opt_all.rst7 -ref ${name}_opt_all.rst7 -o opt_temp.out -r ${name}_opt_temp.rst7 -x opt_temp.mdcoord -inf opt_temp.mdinfo
