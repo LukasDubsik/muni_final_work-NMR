ac1d6e32eb6ad171cc9eb0d74af8310ce7e43839
Lukas Dubsik <lukasdubsik@wolf17>
2025-09-21 11:22:35 +0200

    Fix for working gaussian

---
+++ pool.sh
@@ -0,0 +1,19 @@
+#!/bin/.bash
+
+while true; do
+
+    #Save the current position of the cursor
+    printf '\033[s'
+
+    #print the current status of the jobs
+    pqstat
+
+    #Wait for a time
+    sleep 10
+
+    #Return to the original position
+    printf '\033[u'
+    #Clear from there to the end of the screen
+    printf '\033[J'
+
+done
