bb4ddabfa7fa2e283d58e8cbaa44059e4c144230
LukasDubsik <luke@bubaci.net>
2025-10-23 08:58:57 +0200

    tleap rev 3

--- scripts_meta/tleap.sh
+++ scripts_meta/tleap.sh
@@ -8,7 +8,7 @@ module add amber-14
 
 test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
 
-cp "$DATADIR/{tleap.in, ${name}.frcmod, ${name}_charges_fix.mol2}"  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
+cp "$DATADIR/{tleap.in,${name}.frcmod,${name}_charges_fix.mol2}"  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
 
 cd $SCRATCHDIR 
 
@@ -16,6 +16,6 @@ module add amber-14
 
 tleap -f tleap.in
 
-cp {${name}.parm7, ${name}.rst7} $DATADIR/ || { echo >&2 "Result file(s) copying failed (with a code $?) !!"; exit 4; }
+cp {${name}.parm7,${name}.rst7} $DATADIR/ || { echo >&2 "Result file(s) copying failed (with a code $?) !!"; exit 4; }
 
 clean_scratch
