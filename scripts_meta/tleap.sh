c281cd42a4326d748c2ccafc0af65449b7b8c7a3
LukasDubsik <luke@bubaci.net>
2025-10-23 08:53:27 +0200

    tleat fix directory

--- scripts_meta/tleap.sh
+++ scripts_meta/tleap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-DATADIR=${dir}/process/preparations/antechamber
+DATADIR=${dir}/process/preparations/tleap
 
 echo "$PBS_JOBID is running on node $(hostname -f) in a scratch directory $SCRATCHDIR" >> "$DATADIR/jobs_info.txt"
 
