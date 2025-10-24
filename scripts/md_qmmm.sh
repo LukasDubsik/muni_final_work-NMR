f17ad2cf66eae6a5c343510bcf05f0d164650d6e
LukasDubsik <luke@bubaci.net>
2025-10-24 18:59:39 +0200

    Fixed that gaussian wasn't properly run for QM/MM

--- scripts/md_qmmm.sh
+++ scripts/md_qmmm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env infinity-env
 
 module add amber
+module add gaussian
 
 sander -O -i ${file} -p ${name}.parm7 -c ${name}_opt_pres.rst7 -o ${name}_md.out -r ${name}_md.rst7 -x ${name}_md.mdcrd -inf ${name}_md.mdinfo
