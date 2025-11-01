aaf6f53848957f851cfd99ff05d06f7dc0bb7e79
LukasDubsik <luke@bubaci.net>
2025-11-01 08:44:28 +0100

    Cuda ready

--- scripts/opt_temp.sh
+++ scripts/opt_temp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env infinity-env
 
-module add amber
+module add pmemd-cuda
 
-pmemd -O -i ${file} -p ${name}.parm7 -c ${name}_opt_all.rst7 -ref ${name}_opt_all.rst7 -o opt_temp.out -r ${name}_opt_temp.rst7 -x opt_temp.mdcoord -inf opt_temp.mdinfo
+pmemd.cuda -O -i ${file} -p ${name}.parm7 -c ${name}_opt_all.rst7 -ref ${name}_opt_all.rst7 -o opt_temp.out -r ${name}_opt_temp.rst7 -x opt_temp.mdcoord -inf opt_temp.mdinfo
