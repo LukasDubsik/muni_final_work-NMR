fff3807f5b2b357fb2baa9ba990b3dd33313962c
LukasDubsik <luke@bubaci.net>
2025-11-07 12:23:08 +0100

    Saved scripts and reoranged their organization

--- scripts/metacentrum_scripts/md.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env infinity-env
-
-module add pmemd-cuda
-
-pmemd.cuda -O -i md.in -p ${name}.parm7 -c ${name}_opt_pres.rst7 -o ${name}_md.out -r ${name}_md.rst7 -x ${name}_md.mdcrd -inf ${name}_md.mdinfo
