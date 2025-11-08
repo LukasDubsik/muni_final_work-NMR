acfe06a35cd54c1b6f22e6438e0fa7e4a5b80b1d
LukasDubsik <luke@bubaci.net>
2025-11-08 08:16:10 +0100

    aDDED THE UTILITIES FUNCTIONS FILE

---
+++ lib/general_scripts/bash/functions/utilities.sh
@@ -0,0 +1,10 @@
+# shellcheck disable=SC2148
+# Guard so we don't load twice
+[[ ${_UTILITIES_SH_LOADED:-0} -eq 1 ]] && return
+_UTILITIES_SH_LOADED=1
+
+# ensure_dir DIR_NAME
+# Makes sure the dir exists by creating it
+# Globals: none
+# Returns: Nothing
+ensure_dir() { mkdir -p -- "$1"; }
