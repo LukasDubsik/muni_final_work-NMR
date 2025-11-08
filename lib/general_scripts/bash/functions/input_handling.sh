2fc2e025a10aecaa99b164d071fda3a836570a6e
LukasDubsik <luke@bubaci.net>
2025-11-08 08:15:11 +0100

    Split files into separate libraries

---
+++ lib/general_scripts/bash/functions/input_handling.sh
@@ -0,0 +1,130 @@
+# shellcheck disable=SC2148
+# Guard so we don't load twice
+[[ ${_INPUT_HANDLING_SH_LOADED:-0} -eq 1 ]] && return
+_INPUT_HANDLING_SH_LOADED=1
+
+declare -A Params
+
+read_config() {
+	#Hardwired location of the input file
+	local file="inputs/sim.txt"
+	[[ -f "$file" ]] || die "Config file not found: $file"
+
+	#Iterate the file to get all its values
+	while IFS= read -r line || [[ -n "$line" ]]; do
+		#Skip empty lines and lines starting with # (comments)
+		[[ -z "$line" || ${line:0:1} == '#' ]] && continue
+		#Extract all lines containing assign
+		if [[ "$line" == *":="* ]]; then
+			local key=${line%%:=*}
+			local val=${line#*:=}
+			#Strip all whitespaces from the key
+			key=${key//[[:space:]]/}
+			Params["$key"]=$val
+		fi
+	done <"$file"
+}
+
+get_cfg() {
+	local key=$1
+	if [[ -n "${Params[$key]}" ]]; then 
+		die "Expected: ${Params[$key]}. But it was not present!"; 
+	else 
+		return 1; 
+	fi
+}
+
+check_in_file() {
+	# Check that given in file truly present
+	local name=$1 dir=$2
+	local file="$dir/${name}.in"
+	[[ -f "$file" ]] || die "Missing input template: $file"
+	succ ".in present: $file"
+}
+
+check_sh_file() {
+	# Check that given sh script truly present
+	local name=$1 dir=$2
+	local file="$dir/${name}.sh"
+	[[ -f "$file" ]] || die "Missing script: $file"
+	succ ".sh present: $file"
+}
+
+load_cfg() {
+	#Declare the values as explicitly global
+	declare -g \
+    name save_as input_type gpu meta directory amber_ext \
+    tleap opt_water opt_all opt_temp opt_pres md cpptraj \
+    md_iterations antechamber_cmd parmchk2_cmd
+
+	#See if we have the name for the molecule
+	name=${OVR_NAME:-$(get_cfg 'name')}
+
+	#See if we have the save for the molecule
+	save_as=${OVR_SAVE:-$(get_cfg 'save_as')}
+
+	#Get the input type and check it is valid type
+	input_type=$(get_cfg 'input_type')
+	#Check that it is allowed
+	if [[ ! $input_type == 'mol2' && ! $input_type == '7' ]]; then
+		exit_program "Only allowed input file types are mol2 and rst/parm7!"
+	fi
+
+	#See if we have gpu specified
+	gpu=$(get_cfg 'gpu')
+
+	#If we want to run the code in metacentrum
+	meta=$(get_cfg 'meta')
+
+	#Number of iterations of the md
+	md_iterations=$(get_cfg 'md_iterations')
+
+	info "Config loaded: name=$name, save_as=$save_as, input_type=$input_type, gpu=$gpu, meta=$meta, md iterations=$md_iterations"
+
+	#By default amber extension is empty
+	amber_ext=""
+
+	#If so also see that other important values given
+	if [[ $meta == 'true' ]]; then
+		#What is our directoryt in which we are running the script
+		directory=$(get_cfg 'directory')
+
+		#What version of amber are we using
+		amber_ext=$(get_cfg 'amber')
+
+		info "All the informations for metacentrum loaded correctly: directory=$directory, amber=$amber_ext"
+	fi
+
+	#Additional parametrs for specfic programs - only for mol2
+	if [[ $input_type == "mol2" ]]; then
+		antechamber_cmd=$(get_cfg 'antechamber')
+		parmchk2_cmd=$(get_cfg 'parmchk2')
+
+		info "All the additional parametrs for mol2 loaded correctly"
+	fi
+
+	#Load the names of the .in files (all need to be under inputs/simulation/)
+	tleap=$(get_cfg 'tleap')
+	opt_water=$(get_cfg 'opt_water')
+	opt_all=$(get_cfg 'opt_all')
+	opt_temp=$(get_cfg 'opt_temp')
+	opt_pres=$(get_cfg 'opt_pres')
+	md=$(get_cfg 'md')
+	cpptraj=$(get_cfg 'cpptraj')
+}
+
+check_cfg() {
+	#Directory, where the .in fils must be stored
+	PATH_TO_INPUTS="inputs/simulation"
+
+	#Go file by file and check if they are present
+	check_in_file "$tleap" "$PATH_TO_INPUTS"
+	check_in_file "$opt_water" "$PATH_TO_INPUTS"
+	check_in_file "$opt_all" "$PATH_TO_INPUTS"
+	check_in_file "$opt_temp" "$PATH_TO_INPUTS"
+	check_in_file "$opt_pres" "$PATH_TO_INPUTS"
+	check_in_file "$md" "$PATH_TO_INPUTS"
+	check_in_file "$cpptraj" "$PATH_TO_INPUTS"
+
+	succes "All .in files are present and loaded."
+}
---
+++ lib/general_scripts/bash/functions/modules.sh
@@ -0,0 +1,63 @@
+# shellcheck disable=SC2148
+# Guard so we don't load twice
+[[ ${_MODULES_SH_LOADED:-0} -eq 1 ]] && return
+_MODULES_SH_LOADED=1
+
+# usage MODULE_NAME
+# Tries to add the module.
+# Globals: none
+# Returns: Exits the program if module can't be added, otherwise nothing
+check_module() { module add "$1" > /dev/null 2>&1 || die "Couldn't add the module: $1"; }
+
+check_module_conda() {
+	conda activate "$1" > /dev/null 2>&1 || die "Couldn't activate conda enviroment";
+	module add "$1" > /dev/null 2>&1 || die "Couldn't add the module: $1"; 
+	conda deactivate > /dev/null 2>&1 || die "Couldn't exit conda enviroment";
+}
+
+check_modules() {
+	#Load the parametrs
+	local amber_mod=$1
+	local gauss_mod=$2
+
+	#That crest is available
+	if [[ $meta == "true" ]]; then
+		#On metacentrum crest needs conda additionaly to be run
+		check_module_conda "crest"
+	else
+		check_module "crest"
+	fi
+
+	#That pmemd.cuda is available - only for Wolf side of things
+	if [[ $meta == "false" ]]; then
+		check_module "pmemd-cuda"
+	fi
+
+	#That Amber is available
+	check_module "$amber_mod"
+
+	#That gaussian is available
+	check_module "$gauss_mod"
+
+	succes "All the modules (crest, amber, gaussian) are present"
+}
+
+# check_require FUNCTION_NAME
+# Tries to find the path to the function.
+# Globals: none
+# Returns: Exits if the function can't be run, otherwise nothing.
+check_require() { command -v "$1" >/dev/null 2>&1 || die "Missing required command: $1"; }
+
+check_requires() {
+	#That antechamber is running
+	check_require "antechamber"
+
+	#And so on
+	check_require "parmchk2"
+	check_require "pmemd"
+	check_require "pmemd.cuda"
+	check_require "cpptraj"
+	check_require "gaussian"
+
+	succes "All functions within the modules are present"
+}
