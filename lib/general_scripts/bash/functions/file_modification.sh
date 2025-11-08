7e4bd21a32c25da366fe3201c146f220d33c8ae2
LukasDubsik <luke@bubaci.net>
2025-11-08 11:29:34 +0100

    Substitutions for start and end of the metacentrum files

--- lib/general_scripts/bash/functions/file_modification.sh
+++ lib/general_scripts/bash/functions/file_modification.sh
@@ -3,3 +3,33 @@
 [[ ${_FILE_NMODIFICATION_SH_LOADED:-0} -eq 1 ]] && return
 _FILE_NMODIFICATION_SH_LOADED=1
 
+# substitute_name_sh_start DST COPY DIR JOB
+# Setup the start of the metacentrum job
+# Globals: none
+# Returns: Nothing
+substitute_name_sh_start() {
+	# Sed used to replace the name
+	local dst=$1 copy=$2 dir=$3 job=$4
+	local src="lib/job_scripts/metacentrum_start.txt"
+	[[ -f "$src" ]] || die "Missing template: $src"
+	sed "s/\${copy}/${copy}/g; s/\${dir}/${dir}/g; s/\${job}/${job}/g" "$src" >"$dst" || die "sed couldn't be performed on: $src"
+}
+
+# substitute_name_sh_end DST COPY_END
+# What files to copy back to our working dir from the running dir
+# Globals: none
+# Returns: Nothing
+substitute_name_sh_end() {
+	# Sed used to replace the name
+	local dst=$1 cp_end=$2
+	local src="lib/job_scripts/metacentrum_end.txt"
+	[[ -f "$src" ]] || die "Missing template: $src"
+	sed "s/\${copy_end}/${cp_end}/g" "$src" >"$dst" || die "sed couldn't be performed on: $src"
+}
+
+${copy}
+${job}
+${module}
+${name}
+${file}
+${num}
