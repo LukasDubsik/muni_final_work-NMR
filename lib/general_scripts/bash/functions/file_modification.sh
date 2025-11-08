f7e8314463e20a08a9bf622add15964597e497ae
LukasDubsik <luke@bubaci.net>
2025-11-08 11:42:58 +0100

    Created the start/end and all other substitution files

--- lib/general_scripts/bash/functions/file_modification.sh
+++ lib/general_scripts/bash/functions/file_modification.sh
@@ -3,33 +3,66 @@
 [[ ${_FILE_NMODIFICATION_SH_LOADED:-0} -eq 1 ]] && return
 _FILE_NMODIFICATION_SH_LOADED=1
 
-# substitute_name_sh_start DST COPY DIR JOB
+# substitute_name_sh_meta_start DST COPY DIR JOB
 # Setup the start of the metacentrum job
 # Globals: none
 # Returns: Nothing
-substitute_name_sh_start() {
+substitute_name_sh_meta_start() {
 	# Sed used to replace the name
 	local dst=$1 copy=$2 dir=$3 job=$4
 	local src="lib/job_scripts/metacentrum_start.txt"
+	local dst_full="${dst}/start.txt"
 	[[ -f "$src" ]] || die "Missing template: $src"
-	sed "s/\${copy}/${copy}/g; s/\${dir}/${dir}/g; s/\${job}/${job}/g" "$src" >"$dst" || die "sed couldn't be performed on: $src"
+	sed "s/\${copy}/${copy}/g; s/\${dir}/${dir}/g; s/\${job}/${job}/g" "$src" >"$dst_full" || die "sed couldn't be performed on: $src"
 }
 
-# substitute_name_sh_end DST COPY_END
+# substitute_name_sh_meta_end DST COPY_END
 # What files to copy back to our working dir from the running dir
 # Globals: none
 # Returns: Nothing
-substitute_name_sh_end() {
+substitute_name_sh_meta_end() {
 	# Sed used to replace the name
 	local dst=$1 cp_end=$2
 	local src="lib/job_scripts/metacentrum_end.txt"
+	local dst_full="${dst}/end.txt"
 	[[ -f "$src" ]] || die "Missing template: $src"
-	sed "s/\${copy_end}/${cp_end}/g" "$src" >"$dst" || die "sed couldn't be performed on: $src"
+	sed "s/\${copy_end}/${cp_end}/g" "$src" >"$dst_full" || die "sed couldn't be performed on: $src"
 }
 
-${copy}
-${job}
-${module}
-${name}
-${file}
-${num}
+# substitute_name_sh_wolf_start DST
+# Just copies to the resulting dir the starting wolf file
+# Globals: none
+# Returns: Nothing
+substitute_name_sh_wolf_start() {
+	# Sed used to replace the name
+	local dst=$1
+	local src="lib/job_scripts/wolf_start.txt"
+	local dst_full="${dst}/start.txt"
+	[[ -f "$src" ]] || die "Missing template: $src"
+	cp "$src" "$dst_full" || die "sed couldn't be performed on: $src"
+}
+
+# substitute_name_sh FIL DST MODULE NAME FILE NUM
+# Substitute values directly into the running script of ythe job
+# Globals: none
+# Returns: Nothing
+substitute_name_sh() {
+	# Sed used to replace the name
+	local fil=$1 dst=$2 module=$3 name=$4 file=$5 num=$6
+	local src="lib/job_scripts/programs/${fil}"
+	local dst_full="${dst}/job_file.txt"
+	[[ -f "$src" ]] || die "Missing template: $src"
+	sed "s/\${module}/${module}/g; s/\${name}/${name}/g; s/\${file}/${file}/g; s/\${num}/${num}/g" "$src" >"$dst_full" || die "sed couldn't be performed on: $src"
+}
+
+# construct_sh_wolf DIR NAME
+# Combines the starting file for WOLF cluster with the already substituted file for the job itself
+# 	Thus creating a fully working .sh script ready to be submitted to the cluster.
+# Globals: none
+# Returns: Nothing
+construct_sh_wolf() {
+	local dir=$1 name=$2
+
+	#Create the resulting file
+	touch "${dir}/${name}"
+}
