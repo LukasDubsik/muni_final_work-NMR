8351bd2437d4854965be531d4108b4a642ce8459
LukasDubsik <luke@bubaci.net>
2025-11-09 09:19:05 +0100

    antechmaber job

--- lib/general_scripts/bash/job_creation/preparation.sh
+++ lib/general_scripts/bash/job_creation/preparation.sh
@@ -93,3 +93,43 @@ run_antechamber() {
 	add_to_log "antechamber" "$LOG"
 }
 
+# run_antechamber NAME DIRECTORY META AMBER
+# Runs everything pertaining to antechmaber
+# Globals: none
+# Returns: Nothing
+run_parmchk2() {
+	#Load the inputs
+	local name=$1
+	local directory=$2
+	local meta=$3
+	local amber=$4
+
+	info "Started running parmchk2"
+
+    #Start by converting the input mol into a xyz format -necessary for crest
+	JOB_DIR="process/preparations/parmchk2"
+	ensure_dir $JOB_DIR
+
+	#Constrcut the job file
+	if [[ $meta == "true" ]]; then
+		substitute_name_sh_meta_start "$JOB_DIR" "\$DATADIR/${name}_crest.mol2" "${directory}" "antechamber" ""
+		substitute_name_sh_meta_end "$JOB_DIR"
+		substitute_name_sh "antechamber" "$JOB_DIR" "$amber" "$name" "" ""
+		construct_sh_meta "$JOB_DIR" "antechamber"
+	else
+		substitute_name_sh_wolf_start "$JOB_DIR"
+		substitute_name_sh "antechmaber" "$JOB_DIR" "$amber" "$name" "" ""
+		construct_sh_wolf "$JOB_DIR" "antechamber"
+	fi
+
+    #Run the antechmaber
+    submit_job "$meta" "antechmaber" "$JOB_DIR" 4 4 0 "01:00:00"
+
+	#Check that the final files are truly present
+	check_res_file "${name}_charges.mol2" "$JOB_DIR" "antechamber"
+
+	success "\tantechamber has finished correctly"
+
+	#Write to the log a finished operation
+	add_to_log "antechamber" "$LOG"
+}
