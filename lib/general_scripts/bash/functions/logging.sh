db6e13176362b39f03ec8a66b07fc329bab3d00a
LukasDubsik <luke@bubaci.net>
2025-11-10 14:27:33 +0100

    Function for removing last N lines from the log

--- lib/general_scripts/bash/functions/logging.sh
+++ lib/general_scripts/bash/functions/logging.sh
@@ -54,9 +54,14 @@ add_to_log() {
 }
 
 # remove_run_log LOG_FILE
-# Removes the last 6 liones of log representing one md run so infor about the next may be started
+# Removes the last 6 lines of log representing one md run so infor about the next may be started
 # Globals: none
 # Returns: Nothing
 remove_run_log() {
+	local LOG_FILE=$1
 
+	# shellcheck disable=SC2034
+	for i in {1..6}; do 
+		sed '$d' -i "$LOG_FILE"
+	done
 }
