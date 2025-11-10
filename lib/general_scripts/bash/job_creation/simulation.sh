8b39b1f26a289c784aec81e40174c60c3c1b56ae
LukasDubsik <luke@bubaci.net>
2025-11-10 14:32:21 +0100

    added cpptraj job run

--- lib/general_scripts/bash/job_creation/simulation.sh
+++ lib/general_scripts/bash/job_creation/simulation.sh
@@ -252,7 +252,7 @@ run_md() {
 	fi
 
     #Run the antechmaber
-    submit_job "$meta" "$job_name" "$JOB_DIR" 8 8 3 "24:00:00"
+    submit_job "$meta" "$job_name" "$JOB_DIR" 8 8 2 "24:00:00"
 
 	#Check that the final files are truly present
 	check_res_file "${name}_md.rst7" "$JOB_DIR" "$job_name"
@@ -262,3 +262,56 @@ run_md() {
 	#Write to the log a finished operation
 	add_to_log "$job_name" "$LOG"
 }
+
+# run_cpptraj NAME DIRECTORY META AMBER
+# Run cppytraj and sample the needed atoms/molecules for the gaussian NMR
+# Globals: none
+# Returns: Nothing
+run_cpptraj() {
+	#Load the inputs
+	local name=$1
+	local directory=$2
+	local meta=$3
+	local amber=$4
+
+	local job_name="cpptraj"
+
+	info "Started running $job_name"
+
+    #Start by converting the input mol into a xyz format -necessary for crest
+	JOB_DIR="process/simulation/$job_name"
+	ensure_dir $JOB_DIR
+
+	SRC_DIR_1="process/simulation/md"
+
+	#Copy the data from antechamber
+	move_inp_file "${name}_md.rst7" "$SRC_DIR_1" "$JOB_DIR"
+	move_inp_file "${name}.parm7" "$SRC_DIR_1" "$JOB_DIR"
+
+	#Copy the .in file for tleap
+	substitute_name_in "$job_name" "$JOB_DIR" "$name"
+
+	#Constrcut the job file
+	if [[ $meta == "true" ]]; then
+		substitute_name_sh_meta_start "$JOB_DIR" "${directory}" ""
+		substitute_name_sh_meta_end "$JOB_DIR"
+		substitute_name_sh "$job_name" "$JOB_DIR" "$amber" "$name" "$job_name.in" "" ""
+		construct_sh_meta "$JOB_DIR" "$job_name"
+	else
+		substitute_name_sh_wolf_start "$JOB_DIR"
+		substitute_name_sh "$job_name" "$JOB_DIR" "$amber" "$name" "$job_name.in" "" ""
+		construct_sh_wolf "$JOB_DIR" "$job_name"
+	fi
+
+    #Run the antechmaber
+    submit_job "$meta" "$job_name" "$JOB_DIR" 8 8 0 "02:00:00"
+
+	#Check that the final files are truly present
+	check_res_file "${name}_frame.xyz" "$JOB_DIR" "$job_name"
+
+	success "$job_name has finished correctly"
+
+	#Write to the log a finished operation
+	add_to_log "$job_name" "$LOG"
+}
+
