ddf95ef105dff87913261792b918e75a696c0c59
LukasDubsik <luke@bubaci.net>
2025-11-15 21:05:47 +0100

    Function for plotting resulting spectrum finished

--- lib/general_scripts/bash/job_creation/data.sh
+++ lib/general_scripts/bash/job_creation/data.sh
@@ -3,7 +3,7 @@
 [[ ${_DATA_SH_LOADED:-0} -eq 1 ]] && return
 _DATA_SH_LOADED=1
 
-# run_analysis META NUM_FRAMES
+# run_analysis SIGMA LIMIT
 # Analyze the results from the gauss nmr run and get necessary data
 # Globals: none
 # Returns: Nothing
@@ -49,16 +49,45 @@ run_analysis() {
 	add_to_log "$job_name" "$LOG"
 }
 
+# run_plotting
+# Given the results from analysis of the gaussian runs plot the resulting nmr spectra
+# Globals: none
+# Returns: Nothing
+run_plotting() {
+	#Load the inputs
+	local name=$1
+	local save_as=$2
 
-sed "s/\${name}/${name}/g" $SCRIPTS/plot_nmr.plt > process/spectrum/plotting/plot_nmr.plt || { echo -e "\t\t\t[$CROSS] ${RED} Failed to modify the plot_nmr.plt file!${NC}"; exit 1; }
-#Run the script
-#Finally plot and check presence of the graphic file
-gnuplot plot_nmr.plt || { echo -e "\t\t\t[$CROSS] ${RED} Failed to run gnuplot for NMR spectrum!${NC}"; exit 1; }
-mv "${name}"_nmr.png "${save_as}".png
-cd ../../../ || { echo -e "\t\t\t[$CROSS] ${RED} Failed to return to main directory after plotting!${NC}"; exit 1; }
-if [[ ! -f process/spectrum/plotting/${save_as}.png ]]; then
-    echo -e "\t\t\t[$CROSS] ${RED} Plotting the NMR spectrum failed, no file found!${NC}"
-    exit 1
-else
-    echo -e "\t\t\t[$CHECKMARK] Plotting the NMR spectrum successful."
-fi
+	local job_name="plotting"
+
+	info "Started running $job_name"
+
+    #Start by converting the input mol into a xyz format -necessary for crest
+	JOB_DIR="process/spectrum/$job_name"
+	ensure_dir $JOB_DIR
+
+	SRC_DIR_1="process/spectrum/analysis"
+	SRC_DIR_2="lib/general_scripts/gnuplot"
+
+	move_inp_file "avg.dat" "$SRC_DIR_1" "$JOB_DIR"
+	move_inp_file "plot_nmr.plt" "$SRC_DIR_2" "$JOB_DIR"
+
+	#Move to the directory to run the scripts
+	cd $JOB_DIR || die "Couldn'r enter the $JOB_DIR"
+
+	#Run the script and average the resulting nmr spectra
+	gnuplot plot_nmr.plt
+
+	#Check that the final files are truly present
+	check_res_file "$JOB_DIR/${name}_nmr.png" "$JOB_DIR" "$job_name"
+
+	mv "${name}"_nmr.png "${save_as}".png
+
+	#Return back
+	cd ../../.. || die "Couldn't back to the main directory"
+
+	success "$job_name has finished correctly"
+
+	#Write to the log a finished operation
+	add_to_log "$job_name" "$LOG"
+}
