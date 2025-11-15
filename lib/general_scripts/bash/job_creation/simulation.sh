fd2ca495850d5bb8b5aa887afb368643c2b38b18
LukasDubsik <luke@bubaci.net>
2025-11-15 14:02:27 +0100

    Remove double quotes for correct cp mv

--- lib/general_scripts/bash/job_creation/simulation.sh
+++ lib/general_scripts/bash/job_creation/simulation.sh
@@ -344,14 +344,14 @@ move_finished_job() {
 	local RUN=$1
 
 	#Move the frames from cpptraj to the gauss_prep
-	SRC_DIR="process/simulation/cpptraj"
-	DST_DIR="process/spectrum/gauss_prep"
+	SRC_DIR=process/simulation/cpptraj
+	DST_DIR=process/spectrum/gauss_prep
 
-	cp -r $SRC_DIR/frames/* "$DST_DIR/frames" || exit 1
+	cp -r $SRC_DIR/frames/* $DST_DIR/frames || exit 1
 
 	#Create a directory to save the results into
 	ensure_dir "process/run_$RUN"
 
 	#Move all the files there
-	mv -r process/simulation/* "process/run_$RUN"
+	mv -r process/simulation/* process/run_"$RUN"
 }
