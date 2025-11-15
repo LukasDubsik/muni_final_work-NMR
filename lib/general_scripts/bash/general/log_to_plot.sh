fbf8445138fa3e24af1febb3599061f116e360d7
LukasDubsik <luke@bubaci.net>
2025-11-15 21:00:10 +0100

    Function for analysing data has been dadded

--- lib/general_scripts/bash/general/log_to_plot.sh
+++ lib/general_scripts/bash/general/log_to_plot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 num=1
-sigma=32.2  #Value computed by program Multiwfn for shift
 
 #Extract the NMR data for each *.log file resulting from running Gaussian
 for file in nmr/frame.*.log; do
 
-    awk -v SIGMA_TMS=${sigma} -v LIMIT=${limit} -f gjf_to_plot.awk $file > plots/plot.${num}.dat
+    # shellcheck disable=SC2154
+    awk -v SIGMA_TMS="${sigma}" -v LIMIT="${limit}" -f gjf_to_plot.awk "$file" > plots/plot.${num}.dat
     ((num++))
 
 done
--- lib/general_scripts/bash/job_creation/data.sh
+++ lib/general_scripts/bash/job_creation/data.sh
@@ -11,6 +11,8 @@ run_analysis() {
 	#Load the inputs
 	local meta=$1
 	local num_frames=$2
+	local sigma=$3
+	local limit=$4
 
 	local job_name="analysis"
 
@@ -21,19 +23,27 @@ run_analysis() {
 	ensure_dir $JOB_DIR
 
 	SRC_DIR_1="lib/general_scripts/bash/general"
+	SRC_DIR_2="lib/general_scripts/awk"
+	SRC_DIR_3="process/spectrum/gaussian/nmr"
 
-	move_inp_file "xyz_to_gfj.sh" "$SRC_DIR_1" "$JOB_DIR"
+	substitute_name_sh_data "$SRC_DIR_1/log_to_plot.sh" "$JOB_DIR" "" "$limit" "$sigma"
+	move_inp_file "average_plot.sh" "$SRC_DIR_1" "$JOB_DIR"
+	move_inp_file "gfj_to_plot.awk" "$SRC_DIR_2" "$JOB_DIR"
 
-	#Ensure the final dir exists
-    ensure_dir $JOB_DIR/gauss
+	#Copy the log files from gaussian runs
+	cp -r $SRC_DIR_3 $JOB_DIR
 
-	#Run the bash script
-	cd "$JOB_DIR" || die "Couldn't enter the cpptraj directory"
-	bash xyz_to_gfj.sh
-	cd ../../../ || die "Couldn't return back from the cpptraj dir"
+	#Move to the directory to run the scripts
+	cd $JOB_DIR || die "Couldn'r enter the $JOB_DIR"
+
+	#Run the script and average the resulting nmr spectra
+	bash log_to_plot.sh
+
+	#Return back
+	cd ../../.. || die "Couldn't back to the main directory"
 
 	#Check that the final files are truly present
-	check_res_file "$JOB_DIR/gauss/frame.$num_frames.gfj" "$JOB_DIR" "$job_name"
+	check_res_file "$JOB_DIR/avg.dat" "$JOB_DIR" "$job_name"
 
 	success "$job_name has finished correctly"
 
@@ -42,22 +52,8 @@ run_analysis() {
 }
 
 
-#Combine the resulting files and plot the final spectrum
-echo -e "\t\t Plotting the final NMR spectrum..."
-mkdir -p "process/spectrum/plotting/plots/"
-sigma=32.2 #Assumed solvent TMS shielding constant
-echo -e "\t\t\t[$CHECKMARK] Number of atoms in the molecule set to $limit, sigma for TMS set to $sigma."
-#Copy the .sh and .awk and .plt scripts while replacing the values
-sed "s/\${sigma}/${sigma}/g; s/\${limit}/${limit}/g" $SCRIPTS/log_to_plot.sh > process/spectrum/plotting/log_to_plot.sh || { echo -e "\t\t\t[$CROSS] ${RED} Failed to modify the log_to_plot.sh file!${NC}"; exit 1; }
-cp $SCRIPTS/gjf_to_plot.awk process/spectrum/plotting/gjf_to_plot.awk || { echo -e "\t\t\t[$CROSS] ${RED} Failed to modify the log_to_plot.awk file!${NC}"; exit 1; }
-cp $SCRIPTS/average_plot.sh process/spectrum/plotting/average_plot.sh || { echo -e "\t\t\t[$CROSS] ${RED} Failed to copy the average_plot.sh file!${NC}"; exit 1; }
 sed "s/\${name}/${name}/g" $SCRIPTS/plot_nmr.plt > process/spectrum/plotting/plot_nmr.plt || { echo -e "\t\t\t[$CROSS] ${RED} Failed to modify the plot_nmr.plt file!${NC}"; exit 1; }
-cp -r process/spectrum/NMR/nmr process/spectrum/plotting/.
-echo -e "\t\t\t[$CHECKMARK] All necessary files copied to plotting directory."
 #Run the script
-cd process/spectrum/plotting || { echo -e "\t\t\t[$CROSS] ${RED} Failed to enter the plotting directory!${NC}"; exit 1; }   
-bash log_to_plot.sh || { echo -e "\t\t\t[$CROSS] ${RED} Failed to plot the NMR spectrum!${NC}"; exit 1; }
-echo -e "\t\t\t[$CHECKMARK] Log file converted to plot data."
 #Finally plot and check presence of the graphic file
 gnuplot plot_nmr.plt || { echo -e "\t\t\t[$CROSS] ${RED} Failed to run gnuplot for NMR spectrum!${NC}"; exit 1; }
 mv "${name}"_nmr.png "${save_as}".png
