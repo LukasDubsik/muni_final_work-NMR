a1987b08cfe7f3087dbdd47097a217b7d3ce093e
LukasDubsik <luke@bubaci.net>
2025-12-09 12:22:51 +0100

    Update 2.1 -> Added support for heavy atoms NMR

--- lib/general_scripts/bash/general/xyz_to_gfj.sh
+++ lib/general_scripts/bash/general/xyz_to_gfj.sh
@@ -10,10 +10,10 @@ for file in frames/frame_*.xyz; do
     base=gauss/${bas}
     echo "%chk=${bas}.chk" > "${base}".gjf
     # shellcheck disable=SC2129
-    printf "%s\n" "#P B3LYP/6-31++G(d,p) NMR=(GIAO,ReadAtoms) SCRF=COSMO SCF=(XQC,Tight) Int=UltraFine CPHF=Grid=Ultrafine" >> "${base}".gjf
+    printf "%s\n" "#P B3LYP/GenECP NMR=(GIAO,ReadAtoms) SCRF=COSMO SCF=(XQC,Tight) Int=UltraFine CPHF=Grid=Ultrafine" >> "${base}".gjf
 
     echo "" >> "${base}".gjf
-    echo "${bas} — GIAO NMR (Solute with bound waters)" >> "${base}".gjf
+    echo "${bas} — GIAO NMR (Solute with COSMO)" >> "${base}".gjf
     echo "" >> "${base}".gjf
     echo "$char 1" >> "${base}".gjf
 
@@ -23,13 +23,33 @@ for file in frames/frame_*.xyz; do
             name = $1
             x = $2; y = $3; z = $4
 
-            elem = substr(name, 1, 1)
+			sub(/[0-9].*$/, "", name)
+
+            if (length(name) >= 2 && substr(name,2,1) ~ /[a-z]/) {
+                elem = substr(name, 1, 2)
+            } else {
+                elem = substr(name, 1, 1)
+            }
 
             printf "%-2s %12.6f %12.6f %12.6f\n", elem, x, y, z
         }
         '
     	echo ""
 	} >> "${base}".gjf
+
+	# Split the atoms by basis
+	{
+        echo "H C N O S P F Cl Br I 0"
+        echo "6-31++G(d,p)"
+        echo "****"
+        echo "Au 0"
+        echo "SDD"
+        echo "****"
+        echo ""
+        echo "Au 0"
+        echo "SDD"
+        echo ""
+    } >> "${base}".gjf
 
     # Only the selected H atoms will have NMR computed
     H_ATOMS=$(tail -n +3 "$file" | grep -v 'XP' | head -n "$N_CORE" | awk '{i++; if ($1 ~ /^H/) h=(h?h","i:i)} END{print h}')
