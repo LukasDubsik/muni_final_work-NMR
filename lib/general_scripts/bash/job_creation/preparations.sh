6d1fa125a0e2e7d7e94b31343760851f842cb2ca
LukasDubsik <luke@bubaci.net>
2025-12-22 23:03:26 +0100

    Submit 2.034

--- lib/general_scripts/bash/job_creation/preparations.sh
+++ lib/general_scripts/bash/job_creation/preparations.sh
@@ -348,8 +348,7 @@ run_mcpb() {
 	if [[ -n "$bonded_ids" ]]; then
 		addbpairs_line="add_bonded_pairs"
 		for bid in $bonded_ids; do
-			echo " ${metal_id}-${bid}"
-			addbpairs_line+=" ${metal_id}-${bid}"
+			addbpairs_line="$addbpairs_line ${metal_id}-${bid}"
 		done
 	fi
 
