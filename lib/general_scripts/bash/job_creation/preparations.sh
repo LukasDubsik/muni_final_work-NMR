fc9576b3e69d2e4d2ccc03938acf69901d1cf8ac
LukasDubsik <luke@bubaci.net>
2025-12-23 07:57:15 +0100

    Submit 2.040

--- lib/general_scripts/bash/job_creation/preparations.sh
+++ lib/general_scripts/bash/job_creation/preparations.sh
@@ -667,7 +667,7 @@ EOF
 				JOB_META_SELECT_EXTRA="host_licenses=g16:scratch_local=${scratch_gb}gb"
 			fi
 
-			submit_job "$meta" "$STAGE2_JOB" "$STAGE2_DIR" 32 32 0 "12:00:00"
+			submit_job "$meta" "$STAGE2_JOB" "$STAGE2_DIR" 32 8 0 "12:00:00"
 
 			JOB_META_SELECT_EXTRA="$old_extra"
 
